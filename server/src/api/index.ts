c9e7baf63e4ffa8f0f8cd91392035708596f9c6a
brightchul <abhbtbb@gmail.com>
2020-07-17 09:47:52 +0900

    1ㅊㅏ적인 작업

--- server/src/api/index.ts
+++ server/src/api/index.ts
@@ -1,9 +1,14 @@
 import express from 'express';
 import login from './login';
 import signup from './signup';
+import path from 'path';
 
 const router = express.Router();
+const PUBLIC_PATH = path.join(__dirname, '..', 'public');
 
+router.get('/', (req, res, next) => {
+    res.sendFile(path.join(PUBLIC_PATH, 'index.html'));
+});
 router.use("/login", login.api);
 router.use("/signup", signup.api);
 
