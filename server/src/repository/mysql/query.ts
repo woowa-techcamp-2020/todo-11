2f4fd5a1b4a3d66d6fde9089620f6226f5839ad9
brightchul <abhbtbb@gmail.com>
2020-07-17 13:13:46 +0900

    [modify][#12] 테이블 컬럼에서 int에 (width)제거

--- server/src/repository/mysql/query.ts
+++ server/src/repository/mysql/query.ts
@@ -1,5 +1,5 @@
 const CREATE_MEMBER_TB: string = `CREATE TABLE member_tb (
-    no INT(11) NOT NULL AUTO_INCREMENT,
+    no INT NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
     salt VARCHAR(255) NOT NULL,
@@ -11,7 +11,7 @@ const INSERT_MEMBER_TB: string = `INSERT INTO member_tb(email, password, salt) V
 
 
 const CREATE_GROUP_TB: string = `CREATE TABLE group_tb (
-    no INT(11) NOT NULL AUTO_INCREMENT,
+    no INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(255) NOT NULL,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY(no)
@@ -20,9 +20,9 @@ const CREATE_GROUP_TB: string = `CREATE TABLE group_tb (
 const INSERT_GROUP_TB: string = `INSERT INTO group_tb(title) VALUES(?);`;
 
 const CREATE_GROUP_MEMBER_TB: string = `CREATE TABLE group_member_tb (
-    no INT(11) NOT NULL AUTO_INCREMENT,
-    group_no INT(11) NOT NULL,
-    member_no INT(11) NOT NULL,
+    no INT NOT NULL AUTO_INCREMENT,
+    group_no INT NOT NULL,
+    member_no INT NOT NULL,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY(no),
     FOREIGN KEY(group_no) REFERENCES group_tb(no),
@@ -35,11 +35,11 @@ const INSERT_GROUP_MEMBER_TB: string = `INSERT INTO group_member_tb(group_no, me
 
 
 const CREATE_COLUMN_TB: string = `CREATE TABLE column_tb (
-    no INT(11) NOT NULL AUTO_INCREMENT,
-    order_no INT(11) NOT NULL,
+    no INT NOT NULL AUTO_INCREMENT,
+    order_no INT NOT NULL,
     title VARCHAR(255) NOT NULL,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    group_no INT(11) NOT NULL,
+    group_no INT NOT NULL,
     PRIMARY KEY(no),
     FOREIGN KEY(group_no) REFERENCES group_tb(no)
     ON UPDATE CASCADE
@@ -50,12 +50,12 @@ const INSERT_COLUMN_TB: string = `INSERT INTO column_tb(order_no, title, group_n
 
 // 카드는 해당 컬럼이 삭제되면 다 같이 날아간다.
 const CREATE_CARD_TB: string = `CREATE TABLE card_tb (
-    no INT(11) NOT NULL AUTO_INCREMENT,
+    no INT NOT NULL AUTO_INCREMENT,
     content VARCHAR(255) NOT NULL,
-    order_no INT(11) NOT NULL,
+    order_no INT NOT NULL,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    member_no INT(11)  NOT NULL,
-    column_no INT(11) NOT NULL,
+    member_no INT  NOT NULL,
+    column_no INT NOT NULL,
     PRIMARY KEY(no),
     FOREIGN KEY(member_no) REFERENCES member_tb(no),
     FOREIGN KEY(column_no) REFERENCES column_tb(no)
@@ -66,12 +66,12 @@ const CREATE_CARD_TB: string = `CREATE TABLE card_tb (
 const INSERT_CARD_TB: string = `INSERT INTO card_tb(content, order_no, member_no, column_no) VALUES(?, ?, ?, ?);`;
 
 const CREATE_ACTIVITY_TB: string = `CREATE TABLE activity_tb (
-    no INT(11) NOT NULL AUTO_INCREMENT,
-    member_no INT(11) NOT NULL ,
+    no INT NOT NULL AUTO_INCREMENT,
+    member_no INT NOT NULL ,
     action VARCHAR(255) NOT NULL,
-    card_no INT(11) NOT NULL,
-    from_column_no INT(11) NOT NULL,
-    to_column_no INT(11) NOT NULL,
+    card_no INT NOT NULL,
+    from_column_no INT NOT NULL,
+    to_column_no INT NOT NULL,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
 
     PRIMARY KEY(no),
