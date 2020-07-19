fac6883e566c9f86351aeef232bd494e2eba0d39
brightchul <abhbtbb@gmail.com>
2020-07-19 14:21:37 +0900

    [modify][#20] DB에 is_deleted 추가
    삭제 관련해서 is_deleted (bool)를 추가했다.
    bool 타입은 mysql에서 tinyint로 표현된다.
    
    모든 테이블이 다 넣었기 때문에 변경사항이 추가로 생겼다.
    
    mysql.collection의 모든 모델들은 전부 is_deleted가 추가 되었다.
    mysql.repository 로그인 로직에서 is_deleted가 반영되었다.
    query.ts에서 is_deleted 관련해서 쿼리가 수정되었다.
    
    테스트 관련해서도 추가로 변경이 되었다.
    먼저 로그인 로직에서 is_deleted를 체크하도록 변경되었다.
    또한 그에 대한 테스트도 추가되었다.
    
    mysql.repository에서 getMemberInfo의 경우 삭제된 유저에 대해서도 모델을 반환하도록 만들었다. 이렇게 한 이유는 삭제된 유저라도 일단 관리자 입장에서는 쿼리가 아니라 값을 뽑아서 다뤄야 할 경우가 생길수 있기 때문이다.

--- server/src/__tests__/login.test.ts
+++ server/src/__tests__/login.test.ts
@@ -4,18 +4,28 @@ process.env.NODE_ENV = 'test';
 import request from 'supertest';
 import app from '..';
 import {makeSalt, convertPassword} from '../service/pwCrypto';
-import {resetTable, addMember, MemberModel, poolEnd} from '../repository/mysql';
+import {resetTable, addMember, MemberModel, poolEnd, addMemberDeleted} from '../repository/mysql';
 
 const TEST_EMAIL = "abc@abc.com";
 const TEST_PASSWORD = "abc";
 
+const TEST_EMAIL_DELETED = "deleted@abc.com";
+const TEST_PASSWORD_DELETED = "deleted";
+
 beforeAll(async () => {
     await resetTable('member_tb');
-    const result: {password: string, salt: string} = await convertPassword(TEST_PASSWORD);
+    let result: {password: string, salt: string} = await convertPassword(TEST_PASSWORD);
     const member = new MemberModel({
-        no: 0, email:TEST_EMAIL, password: result.password, salt:result.salt, created_at : new Date()
+        no: 0, email:TEST_EMAIL, password: result.password, salt:result.salt, created_at : new Date(), is_deleted:false
     });
+
+    result = await convertPassword(TEST_PASSWORD_DELETED);
+    const deletedMember = new MemberModel({
+        no: 0, email:TEST_EMAIL_DELETED, password: result.password, salt:result.salt, created_at : new Date(), is_deleted:true
+    });
+
     await addMember(member);
+    await addMemberDeleted(deletedMember);
 });
 
 describe('POST /login은', () => {
@@ -48,6 +58,11 @@ describe('POST /login은', () => {
                 .send({email: TEST_EMAIL, password :`${TEST_PASSWORD}1241241}`})
                 .expect(400).end(done);
         });
+        test('삭제된 유저', done => {
+            request(app).post('/login')
+                .send({email: TEST_EMAIL_DELETED, password :TEST_PASSWORD_DELETED})
+                .expect(400).end(done);
+        })
     });
 });
 
