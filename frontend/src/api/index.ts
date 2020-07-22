decc1e2dc6475d18a37b5d96a9f388326808f8dd
hanSong2 <rlagksd@naver.com>
2020-07-22 14:40:28 +0900

    [add] 카드 수정 api 추가

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -1,25 +1,36 @@
+import { CardModel, ColumnModel } from "../model";
+
 const baseUrl = "http://localhost:3000";
 
 export default {
     async login(email: string, password: string) {
-        const res = await fetch(`${baseUrl}/login`, {
+        return await fetch(`${baseUrl}/login`, {
             method: "post",
             headers: {
                 "Content-Type": "application/json",
             },
             body: JSON.stringify({ email, password }),
         });
-        return res;
     },
 
     async signup(email: string, password: string) {
-        const res = await fetch(`${baseUrl}/signup`, {
+        return await fetch(`${baseUrl}/signup`, {
             method: "post",
             headers: {
                 "Content-Type": "application/json",
             },
             body: JSON.stringify({ email, password }),
         });
-        return res;
     },
+
+    async editCardContent(card: CardModel) {
+        return await fetch(`${baseUrl}/todolist/card`, {
+            method: "put",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(card),
+        });
+    },
+    async editColumnTitle(column: ColumnModel) {},
 };
